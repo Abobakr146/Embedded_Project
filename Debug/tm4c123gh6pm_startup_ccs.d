149fc74293041bb2ea83a6e58357a0939fd4176d
Abobakr Mostafa Sayed Abdulrahman <abobakr.abdelrahman45@gmail.com>
2025-04-20 18:58:17 +0200

    Added CCS project

---
+++ Debug/tm4c123gh6pm_startup_ccs.d
@@ -0,0 +1,36 @@
+# FIXED
+
+tm4c123gh6pm_startup_ccs.obj: ../tm4c123gh6pm_startup_ccs.c
+tm4c123gh6pm_startup_ccs.obj: C:/ti/ccs2011/ccs/tools/compiler/include/stdint.h
+tm4c123gh6pm_startup_ccs.obj: C:/ti/ccs2011/ccs/tools/compiler/include/_ti_config.h
+tm4c123gh6pm_startup_ccs.obj: C:/ti/ccs2011/ccs/tools/compiler/include/linkage.h
+tm4c123gh6pm_startup_ccs.obj: C:/ti/ccs2011/ccs/tools/compiler/include/_stdint40.h
+tm4c123gh6pm_startup_ccs.obj: C:/ti/ccs2011/ccs/tools/compiler/include/sys/stdint.h
+tm4c123gh6pm_startup_ccs.obj: C:/ti/ccs2011/ccs/tools/compiler/include/sys/cdefs.h
+tm4c123gh6pm_startup_ccs.obj: C:/ti/ccs2011/ccs/tools/compiler/include/sys/_types.h
+tm4c123gh6pm_startup_ccs.obj: C:/ti/ccs2011/ccs/tools/compiler/include/machine/_types.h
+tm4c123gh6pm_startup_ccs.obj: C:/ti/ccs2011/ccs/tools/compiler/include/machine/_stdint.h
+tm4c123gh6pm_startup_ccs.obj: C:/ti/ccs2011/ccs/tools/compiler/include/sys/_stdint.h
+
+../tm4c123gh6pm_startup_ccs.c:
+
+C:/ti/ccs2011/ccs/tools/compiler/include/stdint.h:
+
+C:/ti/ccs2011/ccs/tools/compiler/include/_ti_config.h:
+
+C:/ti/ccs2011/ccs/tools/compiler/include/linkage.h:
+
+C:/ti/ccs2011/ccs/tools/compiler/include/_stdint40.h:
+
+C:/ti/ccs2011/ccs/tools/compiler/include/sys/stdint.h:
+
+C:/ti/ccs2011/ccs/tools/compiler/include/sys/cdefs.h:
+
+C:/ti/ccs2011/ccs/tools/compiler/include/sys/_types.h:
+
+C:/ti/ccs2011/ccs/tools/compiler/include/machine/_types.h:
+
+C:/ti/ccs2011/ccs/tools/compiler/include/machine/_stdint.h:
+
+C:/ti/ccs2011/ccs/tools/compiler/include/sys/_stdint.h:
+
