1fb678b78c22de900ec22697192cebe51d797ae8
Abobakr Mostafa Sayed Abdulrahman <abobakr.abdelrahman45@gmail.com>
2025-05-05 18:55:41 +0300

    Added LCD driver

---
+++ LCD.c
@@ -0,0 +1,106 @@
+#include "LCD.h"
+
+
+/*************
+=> Initialize the LCD (enable and setup ports)
+=> void arguments and return
+*************/
+void LCD_init(){
+
+    // Enable ports B and E
+    SYSCTL_RCGCGPIO_R |= SYSCTL_RCGC2_GPIOB | SYSCTL_RCGC2_GPIOE;
+    while ((SYSCTL_PRGPIO_R & SYSCTL_RCGCGPIO_R) == 0); //delay (wait) till SYSCTL is enabled
+
+    // Control Pins (RS => PE1, RW => GND, E => PE3)
+    GPIO_PORTE_DIR_R |= 0x0A;
+    GPIO_PORTE_DEN_R |= 0X0A;
+    GPIO_PORTE_AFSEL_R &= ~(0x0E);
+    GPIO_PORTE_PCTL_R &= ~(0x0E);
+    GPIO_PORTE_AMSEL_R &= ~(0x0E);
+
+    // Data Pins (D0-D7 => PB0-PB7)
+    GPIO_PORTB_AFSEL_R = 0;
+    GPIO_PORTB_PCTL_R = 0;
+    GPIO_PORTB_AMSEL_R = 0;
+    GPIO_PORTB_DEN_R = 0XFF;
+
+    // Set port direction (input or output) and send commands to clear the screen on start
+    #if (dataBitsMode == 8)
+        GPIO_PORTB_DIR_R = 0xFF;
+        LCD_command(eight_bits_two_lines_mode);
+    #elif (dataBitsMode == 4)
+        GPIO_PORTB_DIR_R |= 0x0F;
+        LCD_command(four_bits_two_lines_mode);
+    #endif
+    LCD_command(cursor_off); // turn off cursor
+    LCD_command(clear_screen); // clear LCD screen on start
+}
+
+/*************
+=> Send commands to the LCD
+=> argument: command built in the LCD as hex code, defined in "LCD.h" file
+=> void return
+*************/
+void LCD_command(uint8 cmd){
+
+    // disable RS and enable E (LCD)
+    GPIO_PORTE_DATA_R &= ~(1<<1);
+    delay_ms(1);
+    GPIO_PORTE_DATA_R |= 1<<3;
+    delay_ms(1);
+
+    // send the command to LCD
+    GPIO_PORTB_DATA_R = cmd;
+    delay_ms(1);
+
+    // disable E (LCD)
+    GPIO_PORTE_DATA_R &= ~(1<<3);
+    delay_ms(1);
+}
+
+
+/*************
+=> Clear LCD screen
+=> void arguments and return
+*************/
+void LCD_clearScreen(){
+    LCD_command(clear_screen);
+}
+
+
+/*************
+=> Display single character on LCD screen
+=> argument: Character to be displayed
+=> void return
+*************/
+void LCD_displayCharacter(uint8 data){
+
+    // Enable RS and E
+    GPIO_PORTE_DATA_R |= 1<<1;
+    delay_ms(1);
+    GPIO_PORTE_DATA_R |= 1<<3;
+    delay_ms(1);
+
+    // Display character
+    GPIO_PORTB_DATA_R = data;
+    delay_ms(1);
+
+    // Disable E
+    GPIO_PORTE_DATA_R &= ~(1<<3);
+    delay_ms(1);
+}
+
+
+/*************
+=> Display string of characters on LCD screen
+=> argument: Constant String address
+=> void return
+*************/
+void LCD_displayString(const uint8 *data){
+    uint8 index = 0;
+    while(data[index] != '\0'){
+        LCD_displayCharacter(data[index]);
+        index++;
+    }
+}
+
---
+++ LCD.h
@@ -0,0 +1,27 @@
+#include "tm4c123gh6pm.h"
+#include "std_types.h"
+
+#ifndef LCD_H_
+#define LCD_H_
+
+#define     dataBitsMode                            8
+#define     eight_bits_two_lines_mode               0x38
+#define     eight_bits_one_line_mode                0x30
+#define     four_bits_two_lines_mode                0x28
+#define     four_bits_one_line_mode                 0x02
+#define     clear_screen                            0x01
+#define     set_cursor                              0x80
+#define     cursor_off                              0x0C
+#define     cursor_on                               0x0E
+#define     cursor_blink                            0x0F
+
+// RS => PE1, RW => GND, E => PE3, D0-D7 => PB0-PB7
+
+void LCD_init();
+void LCD_displayCharacter(uint8 data);
+void LCD_displayString(const uint8 *data);
+void LCD_clearScreen();
+void LCD_goToRowColumn(uint8 row, uint8 column);
+void LCD_command(uint8 cmd);
+
+#endif
