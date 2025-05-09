d3e1e4ecf955d51cbb5de4af32a7188c699ad563
abdulrahman-491 <abdlrahman.atallah@gmail.com>
2025-05-09 17:47:58 +0300

    added bunch of GPS header and implementation files

---
+++ Debug/GPIO.d
@@ -0,0 +1,15 @@
+# FIXED
+
+GPIO.obj: ../GPIO.c
+GPIO.obj: ../GPIO.h
+GPIO.obj: ../tm4c123gh6pm.h
+GPIO.obj: ../std_types.h
+
+../GPIO.c:
+
+../GPIO.h:
+
+../tm4c123gh6pm.h:
+
+../std_types.h:
+
--- Debug/subdir_rules.mk
+++ Debug/subdir_rules.mk
@@ -8,7 +8,7 @@ SHELL = cmd.exe
 %.obj: ../%.c $(GEN_OPTS) | $(GEN_FILES) $(GEN_MISC_FILES)
 	@echo 'Building file: "$<"'
 	@echo 'Invoking: Arm Compiler'
-	"C:/ti/ccs2011/ccs/tools/compiler/bin/armcl" -mv7M4 --code_state=16 --float_support=FPv4SPD16 -me --include_path="C:/Users/aboba/Desktop/Personal/F/ASU/S1-2/embedded/Project/Embedded_Project" --include_path="C:/ti/ccs2011/ccs/tools/compiler/include" --define=ccs="ccs" --define=PART_TM4C123GH6PM -g --gcc --diag_warning=225 --diag_wrap=off --display_error_number --abi=eabi --preproc_with_compile --preproc_dependency="$(basename $(<F)).d_raw" $(GEN_OPTS__FLAG) "$<"
+	"C:/ti/Compilers/bin/armcl" -mv7M4 --code_state=16 --float_support=FPv4SPD16 -me --include_path="D:/College/Current Courses/Embedded/Embedded_Project" --include_path="C:/ti/Compilers/include" --define=ccs="ccs" --define=PART_TM4C123GH6PM -g --gcc --diag_warning=225 --diag_wrap=off --display_error_number --abi=eabi --preproc_with_compile --preproc_dependency="$(basename $(<F)).d_raw" $(GEN_OPTS__FLAG) "$<"
 	@echo 'Finished building: "$<"'
 	@echo ' '
 
