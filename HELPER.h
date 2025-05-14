e5a6e2e4307fc502124837f4745002a36db84b06
Abobakr Mostafa Sayed Abdulrahman <abobakr.abdelrahman45@gmail.com>
2025-05-14 14:13:49 +0300

    Added files

--- HELPER.h
+++ HELPER.h
@@ -8,4 +8,40 @@
 extern void intToStr(uint32 num, uint8* str, uint8* index);
 extern void floatToStr(float64 num, uint8* str, uint8 precision);
 
+#define     HallAB_Lat          30.064321833333345
+#define     HallAB_Long         31.280189833333345
+
+#define     HallCD_Lat          30.063794833333336
+#define     HallCD_Long         31.280445333333332
+
+#define     Credit_Lat          30.063646333333334
+#define     Credit_Long         31.279065666666664
+
+#define     Main_Lat            30.064635833333340
+#define     Main_Long           31.278724833333331
+
+#define     Concrete_Lat        30.064466999999996
+#define     Concrete_Long       31.277844666666663
+
+#define     Playgrounds_Lat     30.063711666666669
+#define     Playgrounds_Long    31.279007499999995
+
+#define     Workshops_Lat       30.064147333333327
+#define     Workshops_Long      31.279089999999996
+
+#define     Arch_Lat            30.064246166666674
+#define     Arch_Long           31.280515833333343
+
+#define     Cafeteria_Lat       30.063928000000000
+#define     Cafeteria_Long      31.280332333333337
+
+#define     Mosque_Lat          30.064685500000010
+#define     Mosque_Long         31.280304500000003
+
+#define     Library_Lat         30.065210999999997
+#define     Library_Long        31.280070500000004
+
+#define     Fountain_Lat        30.065631333333332
+#define     Fountain_Long       31.278481499999990
+
 #endif
--- main.c
+++ main.c
@@ -20,17 +20,81 @@ void main(){
         // //Latitude: ddmm.mmmm  Long: dddmm.mmmm
         uint8 buf[10];
         // // gcvt(GPS_lat, 9, buf);
-        //snprintf(buf, sizeof(buf), "%.5f", GPS_lat);
-        floatToStr(GPS_lat, buf, 15);
+        //snprintf(buf, sizeof(buf), "%.30f", GPS_lat);
+        floatToStr(GPS_lat, buf, 130);
         UART0_SendString(buf);  // Forward to UART0 (PC terminal)
         UART0_SendByte('\n');  // Forward to UART0 (PC terminal)
         UART0_SendByte('\r');
 
-        floatToStr(GPS_long, buf, 15);
+        // LCD_goToRowColumn(0,0);
+        // LCD_displayString("Lat:");
+        // LCD_displayString(buf);
+
+        floatToStr(GPS_long, buf, 130);
         UART0_SendString(buf);  // Forward to UART0 (PC terminal)
         UART0_SendByte('\n');  // Forward to UART0 (PC terminal)
         UART0_SendByte('\r');
         
+        // LCD_goToRowColumn(1,0);
+        // LCD_displayString("Long:");
+        // LCD_displayString(buf);
+
+        // LCD_goToRowColumn(0, 0);
+        // LCD_displayString("Dest:");
+        LCD_clearScreen();
+        LCD_goToRowColumn(1, 0);
+
+        if(GPS_isWithinRadius(GPS_lat, GPS_long, HallAB_Lat, HallAB_Long, 30)){
+            LCD_displayString("Halls A,B");
+        }
+
+        else if(GPS_isWithinRadius(GPS_lat, GPS_long, HallCD_Lat, HallCD_Long, 30)){
+            LCD_displayString("Halls C,D");
+        }
+
+        else if(GPS_isWithinRadius(GPS_lat, GPS_long, Credit_Lat, Credit_Long, 30)){
+            LCD_displayString("Credit Building");
+        }
+
+        else if(GPS_isWithinRadius(GPS_lat, GPS_long, Main_Lat, Main_Long, 30)){
+            LCD_displayString("Main Building");
+        }
+
+        else if(GPS_isWithinRadius(GPS_lat, GPS_long, Concrete_Lat, Concrete_Long, 30)){
+            LCD_displayString("Concrete Building");
+        }
+
+        else if(GPS_isWithinRadius(GPS_lat, GPS_long, Playgrounds_Lat, Playgrounds_Long, 30)){
+            LCD_displayString("Playgrounds");
+        }
+
+        else if(GPS_isWithinRadius(GPS_lat, GPS_long, Workshops_Lat, Workshops_Long, 30)){
+            LCD_displayString("Workshops");
+        }
+
+        else if(GPS_isWithinRadius(GPS_lat, GPS_long, Arch_Lat, Arch_Long, 30)){
+            LCD_displayString("Archeticture");
+        }
+
+        else if(GPS_isWithinRadius(GPS_lat, GPS_long, Cafeteria_Lat, Cafeteria_Long, 30)){
+            LCD_displayString("Cafeteria");
+        }
+
+        else if(GPS_isWithinRadius(GPS_lat, GPS_long, Mosque_Lat, Mosque_Long, 30)){
+            LCD_displayString("Mosque");
+        }
+
+        else if(GPS_isWithinRadius(GPS_lat, GPS_long, Library_Lat, Library_Long, 30)){
+            LCD_displayString("Library");
+        }
+
+        else if(GPS_isWithinRadius(GPS_lat, GPS_long, Fountain_Lat, Fountain_Long, 30)){
+            LCD_displayString("Fountain");
+        }
+        else{
+            LCD_displayString("None");
+        }
+
     }
 
 }
